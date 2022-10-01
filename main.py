a0ebc26085196d64c22dcf94454151919ba7ea3d
midix <76846706+ushu72@users.noreply.github.com>
2022-10-02 02:14:02 +0700

    autoclicker

---
+++ main.py
@@ -0,0 +1,18 @@
+import pyautogui
+import keyboard
+from time import sleep
+
+
+while True:
+    if keyboard.is_pressed('ctrl+shift'):
+        for i in range(3, 0, -1):
+            print(f'Начинаю работу через {i} секунды')
+            sleep(1)
+
+        while 1:
+            pyautogui.click()
+            if keyboard.is_pressed('esc'):
+                print('Работа завершена!')
+                break
+    else:
+        keyboard.wait('ctrl+shift')
